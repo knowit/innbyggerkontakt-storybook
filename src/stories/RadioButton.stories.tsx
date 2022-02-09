d1029cb460e8868302ca25379fb858e9245999fd
monikahm <monika.muzyk@knowit.no>
2022-02-09 14:08:25 +0100

    Make test tsx

--- src/stories/RadioButton.stories.tsx
+++ src/stories/RadioButton.stories.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { RadioButton } from '../components';
+import { RadioButtonProps } from '../components/RadioButton';
 
 export default {
   title: 'Forms/Radio button',
   component: RadioButton,
 };
 
-export const Default = () => <RadioButton />;
+export const Default = (args: RadioButtonProps) => <RadioButton {...args}/>;
+Default.args = {
+  label: 'Label',
+  value: 'Value'
+}
