b49336112c7ed5821e4017fe80ef99d31ad15a57
Adrian Pleym <aspleym@gmail.com>
2022-07-08 12:05:13 +0200

    Fixed after comments

--- src/components/DeviceShare/DeviceShare.tsx
+++ src/components/DeviceShare/DeviceShare.tsx
@@ -17,23 +17,59 @@ const DeviceShare: React.FC<Props> = (props) => {
   return (
     <div className="deviceShareContainer" {...props}>
       <div
-        className="deviceShareElement --mobile"
+        className="deviceShareElement deviceShareElement--mobile"
         style={{
           width: `${mobileShare}%`,
+          minWidth: '2%',
         }}
       >
-        <Smartphone />
-        {props.showValue ? `${mobileShare} %` : null}
+        {mobileShare > 9 ? (
+          <>
+            <DeviceShareText icon={<Smartphone />} share={mobileShare} showValue={props.showValue} />
+            {mobileShare >= 91 ? (
+              <DeviceShareText
+                style={{ position: 'absolute', right: '0.5rem' }}
+                icon={<Computer />}
+                share={100 - mobileShare}
+                showValue={props.showValue}
+              />
+            ) : null}
+          </>
+        ) : null}
       </div>
       <div
-        className="deviceShareElement --desktop"
+        className="deviceShareElement deviceShareElement--desktop"
         style={{
           width: `${100 - mobileShare}%`,
+          minWidth: '2%',
         }}
       >
-        <Computer />
-        {props.showValue ? `${100 - mobileShare} %` : null}
+        {mobileShare < 91 ? (
+          <>
+            <DeviceShareText icon={<Computer />} share={100 - mobileShare} showValue={props.showValue} />
+            {mobileShare < 10 ? (
+              <DeviceShareText
+                style={{ position: 'absolute', left: '0.5rem' }}
+                icon={<Smartphone />}
+                share={mobileShare}
+                showValue={props.showValue}
+              />
+            ) : null}
+          </>
+        ) : null}
       </div>
+    </div>
+  );
+};
+
+const DeviceShareText: React.FC<
+  { icon: JSX.Element; share: number; showValue?: boolean; className?: string } & JSX.IntrinsicElements['div']
+> = (props) => {
+  return (
+    <div {...props} className={`deviceShareText ${props.className}`}>
+      {props.icon}
+      {props.showValue ? `${props.share}%` : null}
+      {props.children}
     </div>
   );
 };
